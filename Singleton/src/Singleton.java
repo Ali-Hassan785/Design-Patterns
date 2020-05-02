d7115f6e19ac6924e4d8ee8e1214bf35c6d3884d
FIRST_NAME LAST_NAME <don@gmail.com>
2020-05-02 10:21:15 +0500

    Test

---
+++ Singleton/src/Singleton.java
@@ -0,0 +1,11 @@
+
+public class Singleton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ali Hassan");
+		System.out.print("Ali Hassan");
+
+	}
+
+}
